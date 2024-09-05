ecfaf32b79addf133f5280cb0882b86c5f7726a3
Lin Htin Nyein <lynnhtinnyein@gmail.com>
2024-09-05 17:52:29 +0700

    remove user relation

--- database/migrations/2024_09_05_070441_create_contracts_table.php
+++ database/migrations/2024_09_05_070441_create_contracts_table.php
@@ -26,7 +26,6 @@ return new class extends Migration
             $table->boolean('is_employer_confirmed');
 
             $table->unsignedBigInteger('created_by');
-            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
 
             $table->timestamps();
         });
